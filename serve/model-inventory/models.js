fea67a823cff9f536477a74db228542a34cc104e
Fedor Baart <fedor.baart@deltares.nl>
2017-06-25 22:30:06 +0200

    permissions

--- serve/model-inventory/models.js
+++ serve/model-inventory/models.js
@@ -4,14 +4,17 @@ var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 var _ = require('lodash');
 
+const s3Url = 'https://s3-eu-west-1.amazonaws.com/model-inventory/movies/';
+
 function listObjects (prefix) {
   return new Promise(function(resolve, reject) {
     var params = {
       Bucket: "model-inventory",
       Prefix: prefix
     };
-    s3.listObjects(params, function(err, data) {
+    s3.listObjectsV2(params, function(err, data) {
       if (err) {
+        console.log('error during list objects', err);
         reject(err);
       } // an error occurred
       else {
@@ -44,33 +47,54 @@ module.exports.index = (event, context, callback) => {
   Promise.all([
     listObjects("meta/"),
     listObjects("movies/")
-  ]).then((result) => {
-    var metas = result[0];
-    var movies = result[1];
-    var models = [];
-    var modelPromises = _.map(
-      // only the non empty files
-      _.filter(
-        metas,
-        (meta) => meta.Size > 0
-      ),
-      (meta) => {
-        // get each object
-        return getObject(meta.Key);
-      }
-    );
-
-    Promise.all(modelPromises)
-      .then(
-        (models) => {
-          const response = {
-            statusCode: 200,
-            body: JSON.stringify({
-              "models": models
-            })
-          };
-          callback(null, response);
+  ])
+    .then((result) => {
+      var metas = result[0];
+      var movies = result[1];
+      var models = [];
+      var modelPromises = _.map(
+        // only the non empty files
+        _.filter(
+          metas,
+          (meta) => meta.Size > 0
+        ),
+        (meta) => {
+          // get each object
+          return getObject(meta.Key);
         }
       );
-  });
+
+      Promise.all(modelPromises)
+        .then(
+          (models) => {
+            _.each(
+              models,
+              (model) => {
+                if (_.has(model, 'uv.src')) {
+                  model.uv.src = s3Url + model.uv.src;
+                }
+              }
+            );
+
+            const response = {
+              statusCode: 200,
+              body: JSON.stringify({
+                "models": models
+              })
+            };
+            callback(null, response);
+          }
+        )
+        .catch(
+          (reason) => {
+            console.log('Could not get objects: ', reason);
+          }
+        );
+    })
+    .catch(
+      (reason) => {
+        console.log('Could not list objects', reason);
+      }
+
+    );
 };
