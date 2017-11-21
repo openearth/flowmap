cdbc1fb184af8e95357343a7fa61c2f796186504
Fedor Baart <fedor.baart@deltares.nl>
2017-11-21 15:19:13 +0100

    Bump version: 0.2.21 → 0.2.22

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='flowmap',
-    version='0.2.21',
+    version='0.2.22',
     description="Command line utility to transform model output into a flowmap that can be used for games or gpu-based visualizations.",
     long_description=readme + '\n\n' + history,
     author="Fedor Baart",
