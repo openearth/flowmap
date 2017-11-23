4e54410271e38eb6b1b28c7ac4c195207d4fcb6d
Fedor Baart <fedor.baart@deltares.nl>
2017-11-23 17:21:05 +0100

    test tracer

--- tests/test_particles.py
+++ tests/test_particles.py
@@ -12,21 +12,28 @@ from flowmap import particles
 class TestParticles(unittest.TestCase):
 
     def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_make_seed(self):
         x = np.arange(1, 10)
         y = np.sqrt(x)
         z = np.zeros_like(x)
         points = np.c_[x, y, z]
         polydata = tvtk.PolyData()
         polydata.points = points
-        seed = particles.make_particles(polydata)
+        self.polydata = polydata
+
+
+    def tearDown(self):
+        pass
+
+    def test_make_seed(self):
+        seed = particles.make_particles(self.polydata)
         assert isinstance(seed, tvtk.PolyData)
 
+    def test_make_tracer(self):
+        tracer = particles.make_tracer_pipeline(
+            polydata=self.polydata,
+            seed=self.polydata
+        )
+        assert isinstance(tracer, tvtk.StreamTracer)
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
